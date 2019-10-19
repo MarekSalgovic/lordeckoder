35905585b9adb449438ec08c7ba26285989dd916
salgovic.marek@gmail.com <salgovic.marek@gmail.com>
2019-10-19 03:26:36 +0200

    chore(code formatting): formatted code, change constant

--- internal/utils.go
+++ internal/utils.go
@@ -1,23 +1,23 @@
 package internal
 
 import (
+	"encoding/base32"
 	"fmt"
 )
 
-func FixDeckcodeLength(dc string) (string){
+func FixDeckcodeLength(dc string) (string) {
 	length := len(dc)
-	if (length % 8 != 0){
-
-		for i:=0; i < length % 8; i++{
-			dc = dc + "="
+	if (length%8 != 0) {
+		for i := 0; i < length%8; i++ {
+			dc = dc + string(base32.StdPadding)
 		}
 	}
 	return dc
 }
 
-func factionIdToString(id int) (Faction, error){
-	factions :=  []Faction{DEMACIA, FRELJORD, IONIA, NOXUS, PILTOVERZAUN, SHADOWISLES}
-	if id >= len(factions){
+func factionIdToString(id int) (Faction, error) {
+	factions := []Faction{DEMACIA, FRELJORD, IONIA, NOXUS, PILTOVERZAUN, SHADOWISLES}
+	if id >= len(factions) {
 		return "", ErrInvalidCode
 	}
 	return factions[id], nil
@@ -27,7 +27,7 @@ func setCardStruct(set, number, count int, faction Faction) Card {
 	setString := fmt.Sprintf("%02d", set)
 	numberString := fmt.Sprintf("%03d", number)
 	return Card{
-		CardCode: setString+string(faction)+numberString,
+		CardCode: setString + string(faction) + numberString,
 		Count:    count,
 	}
 }
--- lordeckoder.go
+++ lordeckoder.go
@@ -9,18 +9,17 @@ type Decoder interface {
 	DecodeDeckcode(dc string) (internal.Deck, error)
 }
 
-type Decode struct{
-	Format int
+type Decode struct {
+	Format  int
 	Version int
 }
 
-func NewDecoder(params ...int) Decode{
-	format := 1
-	version := 1
-	if len(params) > 0{
+func NewDecoder(params ...int) Decode {
+	format, version := 1,1
+	if len(params) > 0 {
 		format = params[0]
 	}
-	if len(params) > 1{
+	if len(params) > 1 {
 		version = params[1]
 	}
 	return Decode{
@@ -29,19 +28,18 @@ func NewDecoder(params ...int) Decode{
 	}
 }
 
-
-func (d *Decode) DecodeDeckcode(dc string) (internal.Deck, error){
+func (d *Decode) DecodeDeckcode(dc string) (internal.Deck, error) {
 	dc = internal.FixDeckcodeLength(dc)
 	bs, err := base32.StdEncoding.DecodeString(dc)
-	if err != nil{
+	if err != nil {
 		return internal.Deck{}, err
 	}
 	bs, err = internal.ParseHeader(bs, d.Format, d.Version)
-	if err != nil{
+	if err != nil {
 		return internal.Deck{}, err
 	}
 	deck, err := internal.ParseByteStream(bs)
-	if err != nil{
+	if err != nil {
 		return internal.Deck{}, err
 	}
 	return deck, nil
