2151be35db20f7aedfb99d19893cb727e3491bac
salgovic.marek@gmail.com <salgovic.marek@gmail.com>
2019-10-22 03:01:31 +0200

    feat(card): add stringer

--- card.go
+++ card.go
@@ -1,9 +1,11 @@
 package lordeckoder
 
+import "fmt"
 
-//func (c Card) String() string{
-//	return fmt.Sprintf("%02d%s%03d", c.Set, c.GetFaction(), c.Number)
-//}
+
+func (c Card) String() string{
+	return fmt.Sprintf("%02d%s%03d", c.Set, c.GetFaction(), c.Number)
+}
 
 func (c Card) GetFaction() (Faction) {
 	factions := []Faction{DEMACIA, FRELJORD, IONIA, NOXUS, PILTOVERZAUN, SHADOWISLES}
